8aa4bcdc57c02df428fcf096b1430b0d013960bb
Nex Zhu <4370605+NexZhu@users.noreply.github.com>
2023-06-12 15:54:54 +0800

    WIP

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,8 @@
-/**
- * Returns greeting to someone.
- *
- * @remarks
- * For more information about this starter template, see: {@link https://github.com/daotl/ts-lib-starter}.
- *
- * @param name - Name of someone
- * @returns Greeting to `name`
- */
-export function hello(name: string): string {
-  return `Hello ${name}!`
-}
+import { argv } from 'node:process'
+import { globby } from 'globby'
+
+console.log(argv.length)
+
+const paths = await globby(['*', '!cake'])
+
+console.log(paths)
